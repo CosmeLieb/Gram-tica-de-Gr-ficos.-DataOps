23719a3244a3af29a9d2555e8a38ca7cdc675f58
Cosme <alu0101161259@ull.edu.es>
2026-03-01 00:02:17 +0000

    Renombrar lab-renta.py a lab_renta.py y actualizar definitions.py

---
+++ definitions.py
@@ -0,0 +1,8 @@
+from dagster import Definitions, load_assets_from_modules, load_asset_checks_from_modules
+#from scripts import test_checks
+import lab_renta
+
+defs = Definitions(
+    assets=load_assets_from_modules([lab_renta]),
+    asset_checks=load_asset_checks_from_modules([lab_renta])
+)
